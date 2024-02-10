ddcc11cca687f6173926745621b331587bf3622a
IgorIlnicki <symbol_t@ukr.net>
2024-02-10 11:42:20 +0200

    A3

--- Mod5_HT1.py
+++ Mod5_HT1.py
@@ -1,48 +1,19 @@
-# def caching_fibonacci(nn):
-#     cache = []
-#     fibonacci(nn, cache)
-# def fibonacci(nn, cache):
-#             count = len(cache)
-#             if len(cache)<2:
-#                 cache = [0, 1]
-#             # Отримати наступні числа у списку 
-#             if (int(cache[count-2])+int(cache[count-1])) < nn:
-#                 cache = cache + [int(cache[count-2])+int(cache[count-1])]
-#                 return fibonacci(nn, cache) # викликати рекурсивно функцію
-#             else:
-#                 return print("cache = ", cache) # якщо досягнуто кінець, то звичайний вихід
-#             # return cache
-# print("До якого числа знайти числа Фібоначчі ?:")
-# nn = int(input())
-# cache = []
-# fibonacci(nn, cache)
-# # caching_fibonacci(nn)
-# # print("cache = ", cache)
+
 def caching_fibonacci():
-    cache = []
-    print(f"n = {n} cache = {cache}")
-    def fibonacci(n, cache) -> int:
-        count = len(cache)
-        if len(cache)<2:
-            cache = [0, 1]
-            print(f"2   n = {n} cache = {cache}")
-            print(f"3 cache[0] = {cache[0]} cache[{1}] = {cache[1]}")
-            print(f"5 cache[{count-2}] = {cache[count-2]} cache[{count-1}] = {cache[count-1]}")
-            # Отримати наступні числа у списку 
-            b = int(cache[count-2]) + int(cache[count-1])
-            print(f"6 b = {b}")
-        if b < n:
-            cache.append(b)
-            print(f"7   n = {n} cache = {cache}")
-            return fibonacci(n, cache) # викликати рекурсивно функцію
-        else:
-            return print("cache = ", cache) # якщо досягнуто кінець, то звичайний вихід
-    return fibonacci(n, cache)
+    cache = {}  #словник cache для зберігання результатів обчислення
+    def fibonacci(n):
+        if n == 0: return 0
+        if n == 1: return 1
+        if n in cache: return cache[n]
+        cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+        print(" 1 cache = ", cache)
+        return cache[n]
+        
+    return fibonacci(n)
 
 def main(n):
-    # print(f"{cache}") 
     fib = caching_fibonacci()
-    print(f"Числа Фібоначчі для {n}: {fib}")
+    print(f"Числo Фібоначчі для {n}: {fib}")
 
 n = int(input())
 if __name__ == '__main__':
--- Mod5_HT2.py
+++ Mod5_HT2.py
@@ -1,20 +1,19 @@
 
-def my_generator(n):
- 
-    # Инициализируем счетчик
-    value = 0
- 
-    # Цикл выполняется до тех пор, пока счетчик не станет меньше n
-    while value < n:
- 
-        # Возвращаем текущее значение счетчика
-        yield value
- 
-        # Увеличиваем счетчик
-        value += 1
- 
-# Выполняем итерацию генератора
-for value in my_generator(3):
- 
-    # Выводим каждое значение, полученное от генератора
-    print(value)
+text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+char1 = 0
+def generator_numbers(text: str):
+    elements = text.split()
+    print(f" elements =  {elements}")
+    for element in elements:
+        print(f" element =  {element}")
+    # print(f" element2 =  {elements[7]}")
+        if element.isdigit():
+            print(f"Так")
+            element = int(element)
+            print(f" element3 =  {element}")
+            # char1 = char1 + char
+        # yield 
+    # print(f" char1 = {char1}")
+# def msum_profit(text: str, func: Callable):
+generator_numbers(text)
+
